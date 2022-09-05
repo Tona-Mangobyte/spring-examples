45800ef7314f1f1baca01d08be9415f2561dfcfb
Tona <tona@mango-byte.com>
2022-09-05 15:59:14 +0700

    init first module

---
+++ spring-redis/src/main/java/com/mb/AppRedis.java
@@ -0,0 +1,13 @@
+package com.mb;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppRedis
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
